a439af4fa7c6a058cac7a727fc7497af701866fd
Tim Herd <rz@eqdw.net>
2013-04-29 19:34:12 -0700

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DiplomacyTurns::Application.config.secret_token = '913b0c250b9149917e14fb7b4e4d3e42ffe954a6eb151e67623467e3481e440e45aa30b6eaaf809b1401a00b000bf681937a044ab4c3c7cd49616f4e3eb21aed'
